141f84656794181664d3b7890e5c43afaa6126f8
Glonyx <phoenixpua23@gmail.com>
2022-04-01 15:20:05 -0700

    start code

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const myStory = { 
+    // holds story text and choices
+    opener:{
+        text: `All your life, you lived in dullness and settled for the most comfortable option. You work a nine-to-five corporate job making enough to get by a year, living in a one-bedroom studio with no furniture other than a futon. You’ve decided it’s time for a change, and you go outside for a hike around the very mundane, stale, and boring town of Mungville, where everyone lives the same boring life, seeking only comfort in what is attainable. As you are walking, you end up witnessing a glowing dark portal with an odd heat coming off of its exterior. What do you do?`,
+        choice: []
+    }
 
+}
